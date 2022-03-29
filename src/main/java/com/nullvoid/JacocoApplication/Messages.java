0c5352de2b9538ebdbc5ebff0ef01cabc11ab89a
Nikko <nickouchiha5@gmail.com>
2022-03-29 23:04:48 +0800

    Jacoco Code Coverage 2022

---
+++ src/main/java/com/nullvoid/JacocoApplication/Messages.java
@@ -0,0 +1,18 @@
+package com.nullvoid.JacocoApplication;
+
+public class Messages {
+
+    public String getMessage(String name)
+    {
+        StringBuilder s = new StringBuilder();
+        if(name == null || name.trim().length() == 0)
+        {
+            s.append("Please Provide a name!");
+        }
+        else
+        {
+            s.append("Hello ").append(name).append("!");
+        }
+        return s.toString();
+    }
+}
---
+++ src/test/java/com/nullvoid/JacocoApplication/JacocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullvoid.JacocoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
